97fc883b3a004ebab749d922fb38520c3c9ed051
Juan Cruz <jc23729@gmail.com>
2020-12-03 21:26:18 +0000

    I rewrote it by hand and some of the wording

--- main.py
+++ main.py
@@ -39,3 +39,23 @@ if choice1 == "left":
     print("You get attacked by an angry trout. Game Over.")
 else:
   print("You fell into a hole. Game Over.")
+
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if choice2 == "wait":
+    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if choice3 == "red":
+      print("Your in a room full of fire. Game Over")
+    elif choice3 == "yellow":
+      print("You made it. You found the treasure. YOU WIN!")
+    elif choice3 == "blue":
+      print("You entered a room full of beast. Game over")
+    else:
+        print("You chose a door that doesn't exist. Game over")
+        
+  else:
+      print("You got attacked by an ugly trout game over")
+
+else:
+  print("You fell into a hole game over")
